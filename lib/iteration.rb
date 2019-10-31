41e961ef7757a27bc58608c24bb6840a59f8fa24
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-31 07:44:49 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -2,7 +2,7 @@ def join_ingredients(src)
   empty_array=[]
   counter=0 
 while counter < src.length do
-  empty_array << "\"I love #{src[counter][0]} and #{src[counter][1]} on my pizza\""
+  empty_array << "I love #{src[counter][0]} and #{src[counter][1]} on my pizza"
   counter =+ 1 
 end
   return 
@@ -18,13 +18,12 @@ end
 return empty_array
 
 def total_even_pairs(src)
- idx=0 
- total=0 
- while idx < src.length do 
-   if src[idx][0].even? && src[idx][1].even?
-     total =+ src[idx.sum]
+idx=0 
+total=0 
+while idx < src.length do 
+  if src[idx][0].even? && src[idx][1].even?
+    total =+ src[idx.sum]
 end
-idx =+ 1 
-end
-total
+
+return total
 end
